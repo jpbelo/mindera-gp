22188f28949e57374541542a77dc8d8af64c934c
João Belo <jpasbelo@gmail.com>
2018-05-01 21:19:14 +0100

    testing with jest works. Components using redux still don't

--- MinderaGP/components/__tests__/about-test.js
+++ MinderaGP/components/__tests__/about-test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Vacancies from '../vacancies';
+import About from '../about';
 
 import renderer from 'react-test-renderer';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<Vacancies />).toJSON();
+  const tree = renderer.create(<About />).toJSON();
   expect(tree).toMatchSnapshot();
 });
